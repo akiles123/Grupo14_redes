554b378f798d586d59bd23fbe7f1dbbb57f4ecc6
SantiDKLT <101186059+SantiDKLT@users.noreply.github.com>
2023-08-17 21:25:46 -0300

    Update Cliente.java ;)

--- Cliente.java
+++ Cliente.java
@@ -1,50 +1,72 @@
-import socket
-import threading
-class Cliente:
-        def _init_(self, host, puerto):
-        self.host = host
-        self.puerto = puerto
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 
-        def iniciar(self):
-        self.socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  // Crea un socket TCP
-        self.socket_cliente.connect((self.host, self.puerto))  // Se conecta al servidor del broker
-        self.hilo_escucha = threading.Thread(target=self.escuchar_mensajes)  // Crea un hilo para escuchar mensajes
-        self.hilo_escucha.start()  // Inicia el hilo de escucha
+public class Cliente {
+    private static final String HOST = "127.0.0.1";
+    private static final int PUERTO = 8888;
 
-        def escuchar_mensajes(self):
-        while True:
-        datos = self.socket_cliente.recv(1024).decode()  // Recibe datos del servidor
-        if datos:
-        partes = datos.split(",")  // Divide los datos en partes usando coma como separador
-        comando = partes[0]  // El primer elemento es el comando
+    public void iniciar() {
+        try (Socket socketCliente = new Socket(HOST, PUERTO);
+             PrintWriter escritor = new PrintWriter(socketCliente.getOutputStream(), true);
+             BufferedReader lector = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+             Scanner scanner = new Scanner(System.in)) {
 
-        if comando == "MENSAJE":
-        topico = partes[1]  // El segundo elemento es el tópico del mensaje
-        mensaje = partes[2]  // El tercer elemento es el mensaje en sí
-        print(f"Mensaje recibido en el tópico '{topico}': {mensaje}")
-        self.enviar_ack()  // Envía una confirmación de entrega (ACK)
+            Thread hiloEscucha = new Thread(() -> escucharMensajes(lector));
+            hiloEscucha.start(); // Iniciar hilo de escucha de mensajes
 
-        def suscribir(self, topico):
-        self.socket_cliente.send(f"SUSCRIBIR,{topico}".encode())  // Envía un comando de suscripción al servidor
+            while (true) {
+                System.out.print("Ingresa 'S' para suscribirte o 'P' para publicar: ");
+                String accion = scanner.nextLine().toUpperCase();
 
-        def publicar(self, topico, mensaje):
-        self.socket_cliente.send(f"PUBLICAR,{topico},{mensaje}".encode())  // Envía un comando de publicación al servidor
+                if (accion.equals("S")) {
+                    System.out.print("Ingresa el tópico al que deseas suscribirte: ");
+                    String topico = scanner.nextLine();
+                    suscribir(escritor, topico); // Suscribir al tópico
+                } else if (accion.equals("P")) {
+                    System.out.print("Ingresa el tópico en el que deseas publicar: ");
+                    String topico = scanner.nextLine();
+                    System.out.print("Ingresa el mensaje que deseas publicar: ");
+                    String mensaje = scanner.nextLine();
+                    publicar(escritor, topico, mensaje); // Publicar un mensaje en el tópico
+                } else {
+                    System.out.println("Acción no válida. Ingresa 'S' para suscribirte o 'P' para publicar.");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-        def enviar_ack(self):
-        self.socket_cliente.send("ACK".encode())  // Envía una confirmación de entrega (ACK) al servidor
+    private void escucharMensajes(BufferedReader lector) {
+        try {
+            String linea;
+            while ((linea = lector.readLine()) != null) {
+                String[] partes = linea.split(",");
+                String comando = partes[0];
+                if (comando.equals("MENSAJE")) {
+                    String topico = partes[1];
+                    String mensaje = partes[2];
+                    System.out.println("Mensaje recibido en el tópico '" + topico + "': " + mensaje);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-        if _name_ == "_main_":
-        cliente = Cliente("127.0.0.1", 8888)  // Crea una instancia del cliente con la dirección del servidor y el puerto
-        cliente.iniciar()  // Inicia la conexión y el hilo de escucha
+    private void suscribir(PrintWriter escritor, String topico) {
+        escritor.println("SUSCRIBIR," + topico); // Enviar solicitud de suscripción al servidor
+        escritor.flush();
+    }
 
-        while True:
-        accion = input("Ingresa 'S' para suscribirte o 'P' para publicar: ").upper()
-        if accion == "S":
-        topico = input("Ingresa el tópico al que deseas suscribirte: ")
-        cliente.suscribir(topico)  // Permite al usuario suscribirse a un tópico
-        elif accion == "P":
-        topico = input("Ingresa el tópico en el que deseas publicar: ")
-        mensaje = input("Ingresa el mensaje que deseas publicar: ")
-        cliente.publicar(topico, mensaje)  // Permite al usuario publicar un mensaje en un tópico
-        else:
-        print("Acción no válida. Ingresa 'S' para suscribirte o 'P' para publicar.")  // Mensaje de acción inválida
+    private void publicar(PrintWriter escritor, String topico, String mensaje) {
+        escritor.println("PUBLICAR," + topico + "," + mensaje); // Enviar mensaje al servidor
+        escritor.flush();
+    }
+
+    public static void main(String[] args) {
+        Cliente cliente = new Cliente();
+        cliente.iniciar(); // Iniciar el cliente
+    }
+}
