e810dc6f5f480379f58ce35203856e4d516735f1
SantiDKLT <101186059+SantiDKLT@users.noreply.github.com>
2023-08-18 11:57:36 -0300

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -2,42 +2,49 @@ import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
+import static java.lang.System.in;
+import static java.lang.System.out;
+
 public class Cliente {
     private static final String HOST = "127.0.0.1";
     private static final int PUERTO = 8888;
+    private Cliente socket;
 
     public void iniciar() {
         try (Socket socketCliente = new Socket(HOST, PUERTO);
              PrintWriter escritor = new PrintWriter(socketCliente.getOutputStream(), true);
              BufferedReader lector = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
-             Scanner scanner = new Scanner(System.in)) {
+             Scanner scanner = new Scanner(in)) {
 
             Thread hiloEscucha = new Thread(() -> escucharMensajes(lector));
             hiloEscucha.start(); // Iniciar hilo de escucha de mensajes
 
             while (true) {
-                System.out.print("Ingresa 'S' para suscribirte o 'P' para publicar: ");
-                String accion = scanner.nextLine().toUpperCase();
+                out.print("Ingresa 'S' para suscribirte o 'P' para publicar: ");
+                String accion = scanner.nextLine();
 
                 if (accion.equals("S")) {
-                    System.out.print("Ingresa el tópico al que deseas suscribirte: ");
+                    out.print("Ingresa el tópico al que deseas suscribirte: ");
                     String topico = scanner.nextLine();
                     suscribir(escritor, topico); // Suscribir al tópico
                 } else if (accion.equals("P")) {
-                    System.out.print("Ingresa el tópico en el que deseas publicar: ");
+                    out.print("Ingresa el tópico en el que deseas publicar: ");
                     String topico = scanner.nextLine();
-                    System.out.print("Ingresa el mensaje que deseas publicar: ");
+                    out.print("Ingresa el mensaje que deseas publicar: ");
                     String mensaje = scanner.nextLine();
                     publicar(escritor, topico, mensaje); // Publicar un mensaje en el tópico
-                } else {
-                    System.out.println("Acción no válida. Ingresa 'S' para suscribirte o 'P' para publicar.");
+                }else if(accion.equals("Salir")){
+                    break;
+                }
+                else {
+                    out.println("Acción no válida. Ingresa 'S' para suscribirte o 'P' para publicar.");
                 }
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
     private void escucharMensajes(BufferedReader lector) {
         try {
             String linea;
@@ -47,7 +54,7 @@ public class Cliente {
                 if (comando.equals("MENSAJE")) {
                     String topico = partes[1];
                     String mensaje = partes[2];
-                    System.out.println("Mensaje recibido en el tópico '" + topico + "': " + mensaje);
+                    out.println("\n Mensaje recibido en el tópico '" + topico + "': " + mensaje);
                 }
             }
         } catch (IOException e) {
@@ -64,9 +71,19 @@ public class Cliente {
         escritor.println("PUBLICAR," + topico + "," + mensaje); // Enviar mensaje al servidor
         escritor.flush();
     }
+    public static void close() {
+        try {
+            in.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public static void main(String[] args) {
         Cliente cliente = new Cliente();
         cliente.iniciar(); // Iniciar el cliente
+        cliente.close();
     }
 }
