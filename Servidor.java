942d7525c7222aa9116dad92e401eab3fd150cbd
SantiDKLT <101186059+SantiDKLT@users.noreply.github.com>
2023-08-17 21:25:12 -0300

    Update Servidor.java :)

--- Servidor.java
+++ Servidor.java
@@ -1,88 +1,80 @@
-import socket
-import threading
-class ServidorBroker:
-        def _init_(self, host, puerto):
-        self.host = host
-        self.puerto = puerto
-        self.topicos = {}  // Diccionario para almacenar los clientes suscritos a cada tópico
-        self.bloqueo = threading.Lock()  // Bloqueo para gestionar el acceso concurrente a los datos
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
-        def iniciar(self):
-        self.socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  // Crea un socket TCP
-        self.socket_servidor.bind((self.host, self.puerto))  // Asocia el socket al host y puerto especificados
-        self.socket_servidor.listen(5)  // Comienza a escuchar por conexiones entrantes
-        print(f"Servidor Broker escuchando en {self.host}:{self.puerto}")
+public class ServidorBroker {
+    private static final int PUERTO = 8888;
+    private Map<String, List<PrintWriter>> topicos = new HashMap<>();
+    private final Object bloqueo = new Object();
 
-        while True:
-        socket_cliente, direccion_cliente = self.socket_servidor.accept()  // Acepta una conexión entrante
-        hilo_cliente = threading.Thread(target=self.manipular_cliente, args=(socket_cliente,))
-        hilo_cliente.start()  // Inicia un hilo para manejar al cliente
+    public void iniciar() {
+        try (ServerSocket servidor = new ServerSocket(PUERTO)) {
+            System.out.println("Servidor Broker escuchando en el puerto " + PUERTO);
 
-        def manipular_cliente(self, socket_cliente):
-        with socket_cliente:
-        direccion_cliente = socket_cliente.getpeername()
-        print(f"Nuevo cliente conectado: {direccion_cliente}")
+            while (true) {
+                Socket socketCliente = servidor.accept(); // Aceptar conexiones entrantes
+                Thread hiloCliente = new Thread(() -> manipularCliente(socketCliente));
+                hiloCliente.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-        while True:
-        try:
-        datos = socket_cliente.recv(1024).decode()  // Recibe datos del cliente
-        if not datos:
-        self.eliminar_cliente(socket_cliente)  // Desconexión del cliente
-        break
+    private void manipularCliente(Socket socketCliente) {
+        try {
+            PrintWriter escritor = new PrintWriter(socketCliente.getOutputStream(), true);
+            BufferedReader lector = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
 
-        partes = datos.split(",")  // Divide los datos en partes usando coma como separador
-        comando = partes[0]  // El primer elemento es el comando
+            InetAddress direccionCliente = socketCliente.getInetAddress();
+            System.out.println("Nuevo cliente conectado: " + direccionCliente.getHostAddress());
 
-        if comando == "SUSCRIBIR":
-        topico = partes[1]  // El segundo elemento es el tópico al que suscribirse
-        self.suscribir(socket_cliente, topico)  // Suscripción a un tópico
+            String linea;
+            while ((linea = lector.readLine()) != null) {
+                String[] partes = linea.split(",");
+                String comando = partes[0];
 
-        elif comando == "PUBLICAR":
-        topico = partes[1]  // El segundo elemento es el tópico al que publicar
-        mensaje = partes[2]  // El tercer elemento es el mensaje a publicar
-        self.publicar(topico, mensaje)  // Publicación de un mensaje en un tópico
+                if (comando.equals("SUSCRIBIR")) {
+                    String topico = partes[1];
+                    suscribir(escritor, topico); // Suscribir al tópico
+                } else if (comando.equals("PUBLICAR")) {
+                    String topico = partes[1];
+                    String mensaje = partes[2];
+                    publicar(topico, mensaje); // Publicar un mensaje en el tópico
+                } else if (comando.equals("ACK")) {
+                    System.out.println("Confirmación de entrega del mensaje recibida");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-        except Exception as e:
-        print(f"Error al manejar al cliente {direccion_cliente}: {e}")
-        self.eliminar_cliente(socket_cliente)
-        break
+    private void suscribir(PrintWriter escritor, String topico) {
+        synchronized (bloqueo) {
+            topicos.putIfAbsent(topico, new ArrayList<>());
+            topicos.get(topico).add(escritor);
+            System.out.println("Cliente suscrito al tópico " + topico);
+        }
+    }
 
-        def suscribir(self, socket_cliente, topico):
-        with self.bloqueo:
-        if topico not in self.topicos:
-        self.topicos[topico] = []
-        self.topicos[topico].append(socket_cliente)
-        print(f"Cliente suscrito al tópico {topico}")
+    private void publicar(String topico, String mensaje) {
+        synchronized (bloqueo) {
+            List<PrintWriter> suscriptores = topicos.get(topico);
+            if (suscriptores != null) {
+                for (PrintWriter suscriptor : suscriptores) {
+                    suscriptor.println("MENSAJE," + topico + "," + mensaje);
+                    suscriptor.flush();
+                    System.out.println("Enviado mensaje al cliente suscrito al tópico " + topico);
+                }
+            } else {
+                System.out.println("No hay clientes suscritos al tópico " + topico);
+            }
+        }
+    }
 
-        def publicar(self, topico, mensaje):
-        with self.bloqueo:
-        if topico in self.topicos:
-        for socket_cliente in self.topicos[topico]:
-        try:
-        socket_cliente.send(f"MENSAJE,{topico},{mensaje}".encode())
-        print(f"Enviado mensaje al cliente suscrito al tópico {topico}")
-        self.esperar_ack(socket_cliente)  // Espera una confirmación de entrega (ACK)
-        except:
-        print(f"Error al enviar mensaje al cliente suscrito al tópico {topico}")
-        else:
-        print(f"No hay clientes suscritos al tópico {topico}")
-
-        def esperar_ack(self, socket_cliente):
-        try:
-        ack = socket_cliente.recv(1024).decode()
-        if ack == "ACK":
-        print("Confirmación de entrega del mensaje recibida")
-        except:
-        pass
-
-        def eliminar_cliente(self, socket_cliente):
-        with self.bloqueo:
-        for topico, clientes in self.topicos.items():
-        if socket_cliente in clientes:
-        clientes.remove(socket_cliente)
-        print(f"Cliente se ha desuscrito del tópico {topico}")
-        print(f"Cliente desconectado: {socket_cliente.getpeername()}")
-
-        if _name_ == "_main_":
-        broker = ServidorBroker("127.0.0.1", 8888)  // Crea una instancia del servidor con dirección y puerto
-        broker.iniciar()  // Inicia el servidor para aceptar conexiones entrantes
+    public static void main(String[] args) {
+        ServidorBroker broker = new ServidorBroker();
+        broker.iniciar(); // Iniciar el servidor Broker
+    }
+}
